d37b2878ca741243d1ecbb0f7cb90b1b395fdb8d
Obionedonthoeme <126437177+Obionedonthoeme@users.noreply.github.com>
2025-02-23 17:44:56 -0500

    Delete patient-portal-backend directory

--- patient-portal-backend/Fhir.js
+++
@@ -1,8 +0,0 @@
-const FHIR = require('fhir.js');
-const client = FHIR({
-  baseUrl: 'https://ehr.example.com/fhir'
-});
-
-client.read({ type: 'Patient', id: '12345' })
-  .then(response => console.log(response.data))
-  .catch(error => console.error(error));
